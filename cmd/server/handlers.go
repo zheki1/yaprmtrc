8943b6b3d27975289f2faa8362ede1107ca36342
zheki1 <skopin.zhek1@gmail.com>
2025-12-24 20:14:13 +0300

    check2

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -180,7 +180,7 @@ func (s *Server) updateHandlerJSON(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Header().Set("Content-Type", "application/json")
+	//w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	//_ = json.NewEncoder(w).Encode(m)
 }
