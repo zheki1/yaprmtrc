aabf38eacbf243619ec0c370358da322d4f2f5aa
zheki1 <skopin.zhek1@gmail.com>
2025-12-24 20:47:08 +0300

    upd

--- cmd/server/handlers.go
+++ cmd/server/handlers.go
@@ -25,7 +25,22 @@ func (s *Server) valueHandlerJSON(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	body, err := io.ReadAll(r.Body)
+	var reader io.ReadCloser
+	switch r.Header.Get("Content-Encoding") {
+	case "gzip":
+		gzr, err := gzip.NewReader(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		defer gzr.Close()
+		reader = gzr
+	default:
+		reader = r.Body
+	}
+	defer r.Body.Close()
+
+	body, err := io.ReadAll(reader)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
