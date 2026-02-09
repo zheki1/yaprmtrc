c201b8fb21cc1de11fc819a6cb2a24a67d79c087
zheki1 <skopin.zhek1@gmail.com>
2026-02-10 02:47:14 +0300

    check #1

--- cmd/agent/agent.go
+++ cmd/agent/agent.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	"github.com/zheki1/yaprmtrc.git/internal/models"
-	"github.com/zheki1/yaprmtrc.git/internal/security"
 )
 
 type Agent struct {
@@ -148,9 +147,9 @@ func (a *Agent) sendMetric(metric models.Metrics, compressReq bool) {
 			req.Header.Set("Accept-Encoding", "gzip")
 		}
 
-		if a.cfg.Key != "" {
-			req.Header.Set("HashSHA256", security.CalcHash(buf.Bytes(), a.cfg.Key))
-		}
+		// if a.cfg.Key != "" {
+		// 	req.Header.Set("HashSHA256", security.CalcHash(buf.Bytes(), a.cfg.Key))
+		// }
 
 		resp, err := a.client.Do(req)
 		if err != nil {
@@ -198,9 +197,9 @@ func (a *Agent) sendBatch(metrics []models.Metrics, compressReq bool) {
 			req.Header.Set("Accept-Encoding", "gzip")
 		}
 
-		if a.cfg.Key != "" {
-			req.Header.Set("HashSHA256", security.CalcHash(buf.Bytes(), a.cfg.Key))
-		}
+		// if a.cfg.Key != "" {
+		// 	req.Header.Set("HashSHA256", security.CalcHash(buf.Bytes(), a.cfg.Key))
+		// }
 
 		resp, err := a.client.Do(req)
 		if err != nil {
