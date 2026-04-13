7ceed1fd40f3325fde55d0a5c7a9181a805d5ec1
zheki1 <skopin.zhek1@gmail.com>
2026-04-14 01:14:01 +0300

    missed comm

--- cmd/staticlint/exitcheck/testdata/src/exitcall/main.go
+++ cmd/staticlint/exitcheck/testdata/src/exitcall/main.go
@@ -6,13 +6,13 @@ import (
 )
 
 func main() {
-	os.Exit(1)
+	os.Exit(1) // want `call to os\.Exit in the main package is not allowed`
 }
 
 func helper() {
-	log.Fatal("bye")
-	log.Fatalf("bye")
-	log.Fatalln("bye")
-	os.Exit(2)
-	panic("oops")
+	log.Fatal("bye")   // want `call to log\.Fatal in the main package is not allowed`
+	log.Fatalf("bye")  // want `call to log\.Fatalf in the main package is not allowed`
+	log.Fatalln("bye") // want `call to log\.Fatalln in the main package is not allowed`
+	os.Exit(2)         // want `call to os\.Exit in the main package is not allowed`
+	panic("oops")      // want `call to panic in the main package is not allowed`
 }
